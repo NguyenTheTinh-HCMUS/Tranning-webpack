e820a1f021f54c978aa74e26ce9d30ef22bcef5a
NguyenTheTinh <tinh121999@.com>
2020-03-03 17:17:02 +0700

    caching vendor

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,28 @@
 const path = require("path");
 
+const VENDOR_LIBS=[
+  'axios',
+  'bootstrap',
+  
+  'jquery',
+  'react',
+  'react-dom',
+  'react-redux',
+  'react-router-dom',
+  'redux',
+  'redux-thunk'
+
+
+]
 const config = {
-  entry: "./src/index.js",
+  entry: {
+    
+    bundle:"./src/index.js",
+    vendor:VENDOR_LIBS
+  
+  },
   output: {
-    filename: "bundle.js",
+    filename: "[name].js",
     path: path.join(__dirname, "dist")
   },
   module: {
